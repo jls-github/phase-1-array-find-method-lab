c0b1dd1209c7316f895b72792e56c7898c9b3e6a
John <souzajohnlee@gmail.com>
2022-03-28 12:32:53 -0700

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // code your solution here
+function superbowlWin(record) {
+    function isWinningYear(year) {
+        return year.result === "W"
+    }
+
+    const winningYearObject = record.find(isWinningYear)
+
+    if (winningYearObject) {
+        return winningYearObject.year
+    }
+}
+
+// function superbowlWin(record) {
+//     return record.find((year) => year.result === "W")?.year
+// }
